3fa4f087200ccba9fc98fbce0c368fe6b26a73fa
Rakshit <rakshitg600@gmail.com>
2025-01-30 22:23:37 +0530

    Revert "changed name from notakto to lastcross"
    This reverts commit 5f58c0af479335ffb1e6e2a31bcb547811ae9148.

--- android/app/src/main/java/com/notakto/MainActivity.kt
+++ android/app/src/main/java/com/notakto/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.lastcross
+package com.notakto
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/notakto/MainApplication.kt
+++ android/app/src/main/java/com/notakto/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.lastcross
+package com.notakto
 
 import android.app.Application
 import com.facebook.react.PackageList
